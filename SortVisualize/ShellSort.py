2791a6172abdcceb2a962b1dddc4edac768dfa3b
Ivan <2937923559@qq.com>
2019-05-17 18:55:46 +0800

    ShellSort has been finished

--- SortVisualize/ShellSort.py
+++ SortVisualize/ShellSort.py
@@ -18,13 +18,21 @@ while length > 0:
 		while(j < len(r.rectangleList)):
 			r.rectangleList[j].color = colorList[i]
 			j = j + length
-	r.visualize()
 	for i in range(length, len(r.rectangleList)):
 		for j in range(i, length-1, -length):
+			# 现在色号储存
+			colorj = r.rectangleList[j].color
+			colorj_length = r.rectangleList[j-length].color
+			# 访问的数组置为红色
+			r.rectangleList[j].color = (0,0,255)
+			r.rectangleList[j-length].color = (0,0,255)
+			# 显示
+			r.visualize()
+			# 设置为原来的色号
+			r.rectangleList[j].color = colorj
+			r.rectangleList[j-length].color = colorj_length
 			if r.rectangleList[j].value < r.rectangleList[j-length].value:
 				r.swapHasNoColorChange(j, j-length)
-				r.visualize()
-				r.visualize()
 			else:
 				break
 	length = length//2
