355f308b0a5c7572527ab37ae301a6eb42a45187
Ivan <2937923559@qq.com>
2019-05-22 20:32:03 +0800

    I have finished QuickSort

--- SortVisualize/QuickSort.py
+++ SortVisualize/QuickSort.py
@@ -19,38 +19,43 @@ def devide(array, leftIndex, rightIndex):
 def conquer(array, leftIndex, rightIndex):
 	pivotIndex = leftIndex
 	pivotValue = array[pivotIndex].value
-	# 将左右指针数据设置为红色
-	array[leftIndex].color = (0,0,255)
+	# 将基准设置为绿色，右指针数据设置为红色
+	array[pivotIndex].color = (0,255,0)
 	array[rightIndex].color = (0,0,255)
 	# 显示
 	r.visualize()
 	while rightIndex > leftIndex:
 		while rightIndex > leftIndex and array[rightIndex].value > pivotValue:
-			# 先前指针数据恢复原色
-			array[rightIndex].color = r.rectangleColorDefault
+			if rightIndex==pivotIndex:
+				pass
+			else:
+				# 先前指针数据恢复原色
+				array[rightIndex].color = r.rectangleColorDefault
 			rightIndex-=1
 			# 现在指针数据设置为红色
 			array[rightIndex].color = (0,0,255)
 			# 显示
 			r.visualize()
 		while rightIndex > leftIndex and array[leftIndex].value <= pivotValue:
-			# 先前指针数据恢复原色	
-			array[leftIndex].color = r.rectangleColorDefault
+			if leftIndex==pivotIndex:
+				pass
+			else:
+				# 先前指针数据恢复原色	
+				array[leftIndex].color = r.rectangleColorDefault
 			leftIndex+=1
 			# 现在指针数据设置为红色
 			array[leftIndex].color = (0,0,255)
 			# 显示
 			r.visualize()
 		r.swapHasNoColorChange(leftIndex, rightIndex)
-		# swap(array, leftIndex, rightIndex)
-	# swap(array, pivotIndex, rightIndex)
+	if leftIndex==pivotIndex:
+		array[leftIndex].color = (0,255,0)
+	else:
+		# 最后的指针数据恢复原色
+		array[leftIndex].color = r.rectangleColorDefault
 	r.swapHasNoColorChange(pivotIndex, rightIndex)
 	return rightIndex
 	
-def swap(array, i, j):
-	temp = array[i]
-	array[i] = array[j]
-	array[j] = temp
 	
 if __name__ =="__main__":
 	r = DataSeq("QuickSort", np.full((600,1100,3),255, np.uint8),sys.argv[1], 0,0,1100,600)
